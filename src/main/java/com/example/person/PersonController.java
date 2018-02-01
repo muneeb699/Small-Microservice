e725a65f13d78e790efc3e180dd1cb8b9a00efd0
Stefan Zimmermann <stefan.zimmermann@ts.fujitsu.com>
2018-02-01 14:35:41 +0200

    First commit

---
+++ src/main/java/com/example/person/PersonController.java
@@ -0,0 +1,18 @@
+package com.example.person;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+  private PersonRepository repository;
+  public PersonController() {
+      repository = new PersonRepository();
+  }
+
+  @RequestMapping("/person")
+  public Person getPerson(
+          @RequestParam(value = "id", required = true) Long id) {
+      return repository.getPerson(id);
+  }
+}
